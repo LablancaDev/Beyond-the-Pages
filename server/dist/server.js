f6bffb0727f29d59176448f213dfa152de092494
LablancaDev <valenciano_david@hotmail.com>
2024-09-24 17:26:37 +0200

    update file server2.ts

--- server/dist/server.js
+++ server/dist/server.js
@@ -1,9 +1,8 @@
-// * SERVIDOR EXPRESS DEL BACKEND *
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import routesServer from './routesServer.js'; //MUY IMPORTANTE al usar ES Modules es necesario para importar correctamente añadir .js ya que Node.js requiere la extensión en el caso de ES Modules.  
+import routesServer from './routesServer.js';
 import connectDB from './config/config.js';
 // Simulación de __dirname en ES Modules
 const __filename = fileURLToPath(import.meta.url);
@@ -13,8 +12,30 @@ const __dirname = path.dirname(__filename);
 connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
-// Middlewares
-app.use(cors()); // Habilitar CORS
+// Middleware para registrar las solicitudes
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+});
+// Configuración de CORS
+const allowedOrigins = [
+    'http://localhost:4173', // Tu frontend local en modo desarrollo
+    'https://beyound-the-pages.vercel.app' // Dominio de producción en Vercel
+];
+app.use(cors({
+    origin: function (origin, callback) {
+        // Permite solicitudes sin origin (como en postman) o en los dominios permitidos
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        }
+        else {
+            console.error(`CORS error: Origin not allowed: ${origin}`); // Log de error de CORS
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true
+}));
 app.use(express.json()); // Parsear JSON
 // Rutas
 app.use('/api/users', routesServer);
