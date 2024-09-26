9a4ee9780c2497f6fb9888f69cc6d115849fdef3
LablancaDev <valenciano_david@hotmail.com>
2024-09-26 16:58:00 +0200

    update file vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,24 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  root: './',  // La raíz es la carpeta "client"
+  base: '/',
   plugins: [react()],
   define: {
     'process.env': process.env
   },
   build: {
-    outDir: './dist',  // Genera los archivos en ./client/dist
-    emptyOutDir: true,  // Limpia el directorio de salida antes de construir
+    outDir: './dist',
+    emptyOutDir: true,
   },
   server: {
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // El backend local en modo desarrollo
+        target: 'http://localhost:5000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
-  base: '/', 
 })
+
