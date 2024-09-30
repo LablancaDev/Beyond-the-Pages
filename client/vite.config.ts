9962643c01b084ad604ad19220d2e9dfa15ab47e
LablancaDev <valenciano_david@hotmail.com>
2024-09-30 23:03:57 +0200

    update file .gitignore

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,20 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  define: {
+    'process.env': process.env
+  },
   build: {
-    outDir: 'dist',  // Aquí generas los archivos de build
+    outDir: 'dist',
     emptyOutDir: true,
   },
   server: {
-    host: true,
+    host:true,
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // Solo para desarrollo local
+        target: 'http://localhost:5000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
 })
+
