87d20822e6fd31db6b1d5285278f02e1e0693c9c
LablancaDev <valenciano_david@hotmail.com>
2024-09-30 10:14:07 +0200

    update file vercel.json

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,26 @@
 import { defineConfig } from 'vite'
-
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
+  base: '/',
+  plugins: [react()],
+  define: {
+    'process.env': process.env
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
   server: {
+    host:true,
+    port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
-        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
-});
-
+})
 
