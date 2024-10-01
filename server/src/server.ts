a949f512d72132e5481fca8bee94994bec95512b
LablancaDev <valenciano_david@hotmail.com>
2024-10-01 22:11:02 +0200

    deploy vercel production

--- server/src/server.ts
+++ server/src/server.ts
@@ -27,8 +27,7 @@ const allowedOrigins = [
     'http://localhost:5173',  // Tu frontend local en modo desarrollo       npm run dev 
     'http://localhost:4173', // Tu frontend local en modo producción        npm run preview
     'http://127.0.0.1:5500', // Origen local que está causando problemas (añadido aquí)
-    'https://beyond-the-pages-mgyqc77jd-davids-projects-5a52dd2e.vercel.app',
-    'https://beyond-the-pages-three.vercel.app' // Dominio de producción en Vercel
+    'https://beyond-the-pages-lovat.vercel.app/',
 ];
 
 // Configuración de CORS
