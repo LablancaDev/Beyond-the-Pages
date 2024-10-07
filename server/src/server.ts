c62cb190c791f2ccc782de040209867afd8504ed
LablancaDev <valenciano_david@hotmail.com>
2024-10-07 17:45:44 +0200

    update styles and design UX

--- server/src/server.ts
+++ server/src/server.ts
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware para registrar las solicitudes
 app.use((req, res, next) => {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);  
     next();
 });
 
@@ -28,13 +28,12 @@ const allowedOrigins = [
     'http://localhost:4173', // Tu frontend local en modo producción        npm run preview
     'http://127.0.0.1:5500', // Origen local que está causando problemas (añadido aquí)
     'https://beyond-the-pages-seven.vercel.app',
-    'https://beyond-the-pages-6devgc4kk-davids-projects-5a52dd2e.vercel.app', // url de prueba
-    'https://beyond-the-pages-kporvhnyh-davids-projects-5a52dd2e.vercel.app',  // url final de produccion
-    'https://beyond-the-pages-56ujxm8yj-davids-projects-5a52dd2e.vercel.app'
+    'https://beyond-the-pages-kporvhnyh-davids-projects-5a52dd2e.vercel.app',  // url de produccion sin estilos 
+    'https://beyond-the-pages-56ujxm8yj-davids-projects-5a52dd2e.vercel.app'    // url de produccion final  
 ];
 
 // Configuración de CORS
-app.use(cors({
+app.use(cors({  
     origin: function (origin, callback) {
         // Permitir solicitudes sin origen (ej. Postman) o si está en la lista de orígenes permitidos
         if (!origin || allowedOrigins.includes(origin)) {
